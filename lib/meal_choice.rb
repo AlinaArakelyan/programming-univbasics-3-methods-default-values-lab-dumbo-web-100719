620ff3924341f6959c1ea86cf1bee15425488dec
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-09-23 21:41:32 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,8 +3,6 @@
 def meal_choice (veg1, veg2, protein = "meat")
   puts "What a nutritious meal!" 
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
-end
-
-def ret_value (veg1, veg2, protein = "meat")
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
+
